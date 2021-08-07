682b3705ebb0f7a7a77c1836ec56e75032c8acd3
Vivek Kaushal <vivek.kaushal@research.iiit.ac.in>
2021-08-07 21:38:03 +0530

    🍏 valid parenthesis

---
+++ easy/valid-parenthesis.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        list <char> st;
+        unordered_map <char, char> openOf;
+        openOf[')'] = '(';
+        openOf['}'] = '{';
+        openOf[']'] = '[';
+        
+        for (char a : s) {
+            
+            // it's an open bracket
+            if (a == '(' or a == '[' or a == '{') {
+                st.push_front(a);
+            }
+            
+            else {
+                
+                // no open brackets in stack
+                if (st.empty()) {
+                    return false;
+                }
+                
+                char top = st.front();
+                
+                if (top == openOf[a]) // bracket match
+                    st.pop_front();
+                
+                else                 // mismatch
+                    return false;
+            }
+        }
+        
+        
+        return (st.empty());
+    }
+};
